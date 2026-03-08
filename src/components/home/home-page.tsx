ed74a5a937dcedca745e7419ef594f058d13bb1b
Andrew Kim <andrewkim@MacBook-Pro-6.local>
2026-03-08 16:40:28 -0500

    Revert "two click"
    This reverts commit 83413ba53794736ae837eab5fd246cfb980160ff.

--- src/components/home/home-page.tsx
+++ src/components/home/home-page.tsx
@@ -1,5 +1,4 @@
 import { AboutSection } from "@/components/home/about-section";
-import { HomeHashScroll } from "@/components/home/home-hash-scroll";
 import { HeroSection } from "@/components/home/hero-section";
 import { IntroGate } from "@/components/home/intro-gate";
 import { ProjectsSection } from "@/components/home/projects-section";
@@ -8,7 +7,6 @@ import { TimelineSection } from "@/components/home/timeline-section";
 export function HomePage() {
   return (
     <>
-      <HomeHashScroll />
       <IntroGate />
       <HeroSection />
       <AboutSection />
